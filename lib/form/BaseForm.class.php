a3dbebba1322a237f725e2d422f6399651e85837
Paweł Dawczak <pawel.dawczak@gmail.com>
2011-07-06 17:43:48 +0200

    Initial commit

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    Base
+ * @subpackage form
+ * @author     Paweł Dawczak pawel.dawczak@gmail.com 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
