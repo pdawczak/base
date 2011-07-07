fb251c48820c6ec46cb283ba095dc84810da647e
Paweł Dawczak <pawel.dawczak@gmail.com>
2011-07-07 21:31:08 +0200

    module for showing pages added

---
+++ apps/frontend/modules/site/actions/actions.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * site actions.
+ *
+ * @package    Base
+ * @subpackage site
+ * @author     Paweł Dawczak pawel.dawczak@gmail.com
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class siteActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->site = SiteTable::getInstance()->findOneByRoute('@homepage');
+  }
+
+  public function executeShow(sfWebRequest $request)
+  {
+    $this->site = $this->getRoute()->getObject();
+  }
+}
