fd1e5d5cd503a3c8fbfe5485ec6c08df6ffcf0de
Paweł Dawczak <pawel.dawczak@gmail.com>
2011-07-12 20:09:49 +0200

    basic backend module for News added

---
+++ apps/backend/modules/news/lib/newsGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * news module configuration.
+ *
+ * @package    Base
+ * @subpackage news
+ * @author     Paweł Dawczak pawel.dawczak@gmail.com
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class newsGeneratorConfiguration extends BaseNewsGeneratorConfiguration
+{
+}
