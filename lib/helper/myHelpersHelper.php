0caa7764266a0e6c55672670a8decf389a8ce20b
Paweł Dawczak <pawel.dawczak@gmail.com>
2011-07-12 17:34:55 +0200

    my_link_to helper updated

--- lib/helper/myHelpersHelper.php
+++ lib/helper/myHelpersHelper.php
@@ -2,9 +2,10 @@
 
 /**
  * @param myRouteObject $o
+ * @param array $options
  * @return string
  */
-function my_link_to($o)
+function my_link_to($o, $options = array())
 {
-  return link_to($o->myGetLinkName(), $o->myGetLinkUrl());
+  return link_to($o->myGetLinkName(), $o->myGetLinkUrl(), $options);
 }
