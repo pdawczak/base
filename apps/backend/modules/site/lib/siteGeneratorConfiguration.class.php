830624f74717d5dd4cc78612df04ee1324596678
Paweł Dawczak <pawel.dawczak@gmail.com>
2011-07-12 17:41:06 +0200

    backend module for managing Site objects added

---
+++ apps/backend/modules/site/lib/siteGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * site module configuration.
+ *
+ * @package    Base
+ * @subpackage site
+ * @author     Paweł Dawczak pawel.dawczak@gmail.com
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class siteGeneratorConfiguration extends BaseSiteGeneratorConfiguration
+{
+}
---
+++ apps/backend/modules/site/lib/siteGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * site module helper.
+ *
+ * @package    Base
+ * @subpackage site
+ * @author     Paweł Dawczak pawel.dawczak@gmail.com
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class siteGeneratorHelper extends BaseSiteGeneratorHelper
+{
+}
