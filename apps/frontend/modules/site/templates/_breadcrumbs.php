6ef01004d6814f4c92ef219dbe2287cdde5c700b
Paweł Dawczak <pawel.dawczak@gmail.com>
2011-07-16 12:50:02 +0200

    isicsBreadcrumbs plugin integrated

---
+++ apps/frontend/modules/site/templates/_breadcrumbs.php
@@ -0,0 +1,6 @@
+<p id="breadcrumbs">
+  Tutaj jesteś:
+  <?php include_component('isicsBreadcrumbs', 'show', array(
+    'root' => array('text' => 'Strona główna', 'uri' => '@homepage')
+  )) ?>
+</p>
